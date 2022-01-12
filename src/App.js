1bbe4c18bc2754b1424e49c5ad7f527379b59d4a
cjustinn <such.justinn@gmail.com>
2022-01-11 22:54:31 -0500

    Implemented schema definitions, basic user db functions, and basic user api routes.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,61 @@ const dotenv = require('dotenv');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-dotenv.config({ path: "../cfg/configuration.env" });
+dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const Database = require('../schemas/Database');
 
+// Listen to API routes.
 app.get("/", (req, res) => {
     res.json("API is running.");
 });
 
-app.listen(PORT, () => {
-    console.log("API is listening on port " + PORT);
+    // User Routes
+// Find a specific user's data by their Firebase UUID.
+app.get('/api/users/:uuid', (req, res) => {
+    const { uuid } = req.params;
+
+    Database.getUserById(uuid).then((user) => {
+        if (user) {
+            res.status(200).json({
+                message: `User found successfully.`,
+                data: user
+            });
+        } else {
+            res.status(404).json({ error: `Could not find any matching users.` });
+        }
+    }).catch((err) => {
+        res.status(500).json({ error: err });
+    })
+
+});
+
+// Add new user data to the database.
+app.post('/api/users', (req, res) => {
+    if (!req.body.userData) {
+        res.status(400).json({ error: `User data must be provided.` });
+    }
+    else {
+
+        Database.addUser(req.body.userData).then((user) => {
+            res.status(201).json({ message: `Successfully saved user data.`, data: user });
+        }).catch((err) => {
+            res.status(400).json({ error: err });
+        });
+
+    }
+});
+
+// Connect to the database and start the server if successful.
+const PORT = process.env.PORT || 8080;
+var dbURL = process.env.MONGO_URL;
+
+Database.connect(dbURL).then(() => {
+    app.listen(PORT, () => {
+        console.log("API is listening on port " + PORT);
+    });
+}).catch((err) => {
+    console.log(`Could not start the API server.\n${err}`);
 });
