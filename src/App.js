36f04977cce74e7a2a30825b348bbf6419eb58bc
cjustinn <such.justinn@gmail.com>
2022-01-18 16:06:25 -0500

    node-fetch will work eventually.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
-const node_fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 const app = express();
 app.use(cors());
@@ -263,7 +263,7 @@ app.post('/api/events', (req, res) => {
         let event = req.body.eventData;
 
         // Take string location, plug it into geocoding api
-        node_fetch.fetch(`${GEOCODE_API}${event.location.replace(' ', '+')}`, { method: "GET" }).then((data) => {
+        fetch(`${GEOCODE_API}${event.location.replace(' ', '+')}`, { method: "GET" }).then((data) => {
             console.log(JSON.stringify(data));
             res.status(200).json({ data: data });
             // Store geocoding result to variable
