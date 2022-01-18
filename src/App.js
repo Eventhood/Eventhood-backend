7f1c42a1af4ce24897628a6b4016e4644bf1fd4b
cjustinn <such.justinn@gmail.com>
2022-01-18 16:03:47 -0500

    Attempt number a billion at getting fetch to work

--- src/App.js
+++ src/App.js
@@ -263,7 +263,7 @@ app.post('/api/events', (req, res) => {
         let event = req.body.eventData;
 
         // Take string location, plug it into geocoding api
-        node_fetch(`${GEOCODE_API}${event.location.replace(' ', '+')}`, { method: "GET" }).then((data) => {
+        node_fetch.fetch(`${GEOCODE_API}${event.location.replace(' ', '+')}`, { method: "GET" }).then((data) => {
             console.log(JSON.stringify(data));
             res.status(200).json({ data: data });
             // Store geocoding result to variable
